64bce6d6fa9aa82f39965cf32240551e2e23d842
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-07-09 14:20:19 +0200

    move logging

--- src/grid_search.cpp
+++ src/grid_search.cpp
@@ -3,14 +3,14 @@
 GridSearch::GridSearch(ModelHandler &model_handler, HarmonicsCalculator calculator, int component, std::pair<double, double> offset_range, std::pair<double, double> slope_range, double offset_granularity, double slope_granularity, std::vector<GridSearchResult> &results, std::vector<std::shared_ptr<AbstractObjective>> &criteria, double estimated_time_per_calc, int total_steps)
     : model_handler_(model_handler), calculator_(calculator), component_(component), offset_range_(offset_range), slope_range_(slope_range), offset_granularity_(offset_granularity), slope_granularity_(slope_granularity), results_(results), criteria_(criteria), estimated_time_per_step_(estimated_time_per_calc), total_steps_(total_steps)
 {
-    logParams();
-    logEstmatedTime();
     run();
 }
 
 void GridSearch::run()
 {
     Logger::info("== Running grid search for harmonic B" + std::to_string(component_) + " ==");
+    logParams();
+    logEstmatedTime();
 
     // Counter for the iteration number
     int iteration = 1;
