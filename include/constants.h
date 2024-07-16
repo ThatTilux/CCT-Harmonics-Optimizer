6e1f7061c71b0f6fe511a39e7f17ca2a663bbda5
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-07-16 14:53:18 +0200

    clean up constants

--- include/constants.h
+++ include/constants.h
@@ -9,18 +9,17 @@ inline const std::string DATA_DIR_PATH = "../data";
 inline const std::string MODEL_OUTPUT_DIR = "./optimized_cct/";
 inline const std::string TEST_DATA_DIR = "../test_data/";
 
-// OPTIMIZER CONSTANTS
+
+// BN OPTIMIZER CONSTANTS
 
 // this will be displayed to the user as the default value and also used in case none is entered
 inline const double DEFAULT_MAX_BN_VALUE = 0.1;
-// max bn value used for the chiSquare optimizer
-inline const double MAX_BN_CHISQUARE = 1;
 // max datapoints before the optimizer moves on to the next harmonic
-inline const int OPTIMIZER_MAX_DATAPOINTS = 7;
-// weight of the chiSquare component in BO objective function
-inline const double CHISQUARE_WEIGHT = 0.01;
-// TODO let the user decide this
-inline const bool USE_BN_OPTIMIZER_IN_CHISQUARED = true;
+inline const int OPTIMIZER_MAX_DATAPOINTS = 10;
+
+
+// GRID SEARCH OPTIMIZER CONSTANTS
+
 // when the step size in the optimizer is 0, this value is used instead
 inline const double OPTIMIZER_DEFAULT_STEP = 0.000001;
 // Initial threshold for all bn values to start the next phase of the grid search optimizer
@@ -34,11 +33,6 @@ inline const int GRID_MIN_STEPS = 64;
 // Time budget in minutes for one grid search in the grid search optimizer
 inline const double TIME_BUDGET_GRID_SEARCH = 1;
 
-// MODEL CONSTANTS
 
-// max offset value allowed to be set in the model handler
-inline const double MAX_OFFSET = 0.05; // 5mm
-// max slope value allowed to be set in the model handler
-inline const double MAX_SLOPE = 0.0001; // 1e-04 m/coil
 
 #endif // CONSTANTS_H
