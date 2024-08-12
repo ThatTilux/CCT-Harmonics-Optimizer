b4fad65bd3872b52600cf5fa28408ced47c79f22
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-08-12 11:03:38 +0200

    add an objective

---
+++ include/an_objective.hh
@@ -0,0 +1,23 @@
+#ifndef AN_OBJECTIVE_HH
+#define AN_OBJECTIVE_HH
+
+#include "abstract_objective.hh"
+
+// Class to define the an value of the component as the objective
+class AnObjective : public AbstractObjective
+{
+public:
+    AnObjective()
+    {
+        label_ = "an";
+    };
+
+    double evaluate(HarmonicsDataHandler harmonics_handler, int component) override
+    {
+        return harmonics_handler.get_an()[component - 1];
+    }
+
+    virtual ~AnObjective(){};
+};
+
+#endif // AN_OBJECTIVE_HH
