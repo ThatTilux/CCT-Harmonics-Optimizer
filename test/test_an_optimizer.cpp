df22b5b1fae8e50b49880a872db596948663ed0a
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-09-11 15:04:00 +0200

    add tests for checkForHarmonicDriveConstraints

--- test/test_an_optimizer.cpp
+++ test/test_an_optimizer.cpp
@@ -9,8 +9,8 @@ class TestAnOptimizer : public AnOptimizer
 public:
     using AnOptimizer::AnOptimizer; // Inherit constructors
 
-    using AnOptimizer::getMainComponent;
     using AnOptimizer::assertAllHarmonicsPresent;
+    using AnOptimizer::getMainComponent;
 };
 
 class AnOptimizerTest : public ::testing::Test
@@ -19,9 +19,11 @@ protected:
     void SetUp() override
     {
         test_file = TEST_DATA_DIR + "quad_test_unoptimized_allA.json";
+        test_file_2 = TEST_DATA_DIR + "quad_test_unoptimized_allA_linear.json";
     }
 
     boost::filesystem::path test_file;
+    boost::filesystem::path test_file_2;
 };
 
 // Simple AnOptimizer test. Optimizes `an` values.
@@ -48,9 +50,29 @@ TEST_F(AnOptimizerTest, AnOptimizer)
     // Ensure all variables are optimized within the max harmonic value
     for (int i = 0; i < an_values.size(); i++)
     {
-        if ((i+1) != main_component)
+        if ((i + 1) != main_component)
         {
             ASSERT_LE(std::abs(an_values[i]), max_harmonic_value);
         }
     }
+}
+
+// Tests checkForHarmonicDriveConstraints function
+TEST_F(AnOptimizerTest, checkForHarmonicDriveConstraints)
+{
+    // Initialize variables
+    ModelHandler model_handler(test_file);
+    ModelHandler model_handler_2(test_file_2);
+    double max_harmonic_value = 1.0;
+
+    // All linear harmonics
+    ASSERT_NO_THROW({
+        TestAnOptimizer optimizer(model_handler, max_harmonic_value);
+        optimizer.optimize();
+    });
+
+    // One linear harmonic`
+    ASSERT_THROW({
+            TestAnOptimizer optimizer(model_handler_2, max_harmonic_value);
+            optimizer.optimize(); }, std::runtime_error);
 }
