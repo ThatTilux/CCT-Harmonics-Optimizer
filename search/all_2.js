5d8c32400b4a2eb8f42a4944ed81c285fef6e8a1
ThatTilux <ThatTilux@users.noreply.github.com>
2025-03-06 10:34:26 +0000

    deploy: ee0cdecd5311372cfb05f446b719e9bd2bd5baf9

--- search/all_2.js
+++ search/all_2.js
@@ -6,12 +6,11 @@ var searchData=
   ['checklengthsanity_3',['checkLengthSanity',['../classGridSearchOptimizer.html#a874b233260131f10ba19fe11e4ea74ac',1,'GridSearchOptimizer']]],
   ['checkmaincomponent_4',['checkMainComponent',['../classAbstractOptimizer.html#ad3bfeb46defc4949e213aeee4fb42398',1,'AbstractOptimizer']]],
   ['closest_5fpoint_5fon_5fline_5',['closest_point_on_line',['../classStatisticalAnalysis.html#a8694c65c2973eca57237c67d28d0f9b4',1,'StatisticalAnalysis']]],
-  ['computecriteria_6',['computeCriteria',['../classGridSearchOptimizer.html#ab7b0fe1f036597187c2268f344b407a3',1,'GridSearchOptimizer']]],
-  ['computegranularities_7',['computegranularities',['../classGridSearchOptimizer.html#a82b0c7bbdcedb368b24aeac87388521a',1,'GridSearchOptimizer::computeGranularities()'],['../classGridSearchOptimizer.html#ad8a00101712ab87eda5f435ccf097d6e',1,'GridSearchOptimizer::computeGranularities(std::pair&lt; double, double &gt; offset_range, std::pair&lt; double, double &gt; slope_range)']]],
-  ['computemagnetellbounds_8',['computeMagnetEllBounds',['../classAbstractOptimizer.html#aea5b126ff26498a07281ffaafa1bedee',1,'AbstractOptimizer']]],
-  ['computenrmse_9',['computeNRMSE',['../classStatisticalAnalysis.html#af79acd4b85d0e9b01a590d8084ae3219',1,'StatisticalAnalysis']]],
-  ['computevariance_10',['computeVariance',['../classStatisticalAnalysis.html#a2c640b3d283bb173b9a962829023a3a7',1,'StatisticalAnalysis']]],
-  ['constants_2eh_11',['constants.h',['../constants_8h.html',1,'']]],
-  ['copymodelwithtimestamp_12',['copymodelwithtimestamp',['../input__output_8cpp.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp'],['../input__output_8h.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp']]],
-  ['criteria_5fvalues_13',['criteria_values',['../structGridSearchResult.html#a01db1db0fa2541e71a971c0af8ebc3f0',1,'GridSearchResult']]]
+  ['computegranularities_6',['computegranularities',['../classGridSearchOptimizer.html#a82b0c7bbdcedb368b24aeac87388521a',1,'GridSearchOptimizer::computeGranularities()'],['../classGridSearchOptimizer.html#ad8a00101712ab87eda5f435ccf097d6e',1,'GridSearchOptimizer::computeGranularities(std::pair&lt; double, double &gt; offset_range, std::pair&lt; double, double &gt; slope_range)']]],
+  ['computemagnetellbounds_7',['computeMagnetEllBounds',['../classAbstractOptimizer.html#aea5b126ff26498a07281ffaafa1bedee',1,'AbstractOptimizer']]],
+  ['computenrmse_8',['computeNRMSE',['../classStatisticalAnalysis.html#af79acd4b85d0e9b01a590d8084ae3219',1,'StatisticalAnalysis']]],
+  ['computevariance_9',['computeVariance',['../classStatisticalAnalysis.html#a2c640b3d283bb173b9a962829023a3a7',1,'StatisticalAnalysis']]],
+  ['constants_2eh_10',['constants.h',['../constants_8h.html',1,'']]],
+  ['copymodelwithtimestamp_11',['copymodelwithtimestamp',['../input__output_8cpp.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp'],['../input__output_8h.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp']]],
+  ['criteria_5fvalues_12',['criteria_values',['../structGridSearchResult.html#a01db1db0fa2541e71a971c0af8ebc3f0',1,'GridSearchResult']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -4,10 +4,9 @@ var searchData=
   ['checklengthsanity_1',['checkLengthSanity',['../classGridSearchOptimizer.html#a874b233260131f10ba19fe11e4ea74ac',1,'GridSearchOptimizer']]],
   ['checkmaincomponent_2',['checkMainComponent',['../classAbstractOptimizer.html#ad3bfeb46defc4949e213aeee4fb42398',1,'AbstractOptimizer']]],
   ['closest_5fpoint_5fon_5fline_3',['closest_point_on_line',['../classStatisticalAnalysis.html#a8694c65c2973eca57237c67d28d0f9b4',1,'StatisticalAnalysis']]],
-  ['computecriteria_4',['computeCriteria',['../classGridSearchOptimizer.html#ab7b0fe1f036597187c2268f344b407a3',1,'GridSearchOptimizer']]],
-  ['computegranularities_5',['computegranularities',['../classGridSearchOptimizer.html#a82b0c7bbdcedb368b24aeac87388521a',1,'GridSearchOptimizer::computeGranularities()'],['../classGridSearchOptimizer.html#ad8a00101712ab87eda5f435ccf097d6e',1,'GridSearchOptimizer::computeGranularities(std::pair&lt; double, double &gt; offset_range, std::pair&lt; double, double &gt; slope_range)']]],
-  ['computemagnetellbounds_6',['computeMagnetEllBounds',['../classAbstractOptimizer.html#aea5b126ff26498a07281ffaafa1bedee',1,'AbstractOptimizer']]],
-  ['computenrmse_7',['computeNRMSE',['../classStatisticalAnalysis.html#af79acd4b85d0e9b01a590d8084ae3219',1,'StatisticalAnalysis']]],
-  ['computevariance_8',['computeVariance',['../classStatisticalAnalysis.html#a2c640b3d283bb173b9a962829023a3a7',1,'StatisticalAnalysis']]],
-  ['copymodelwithtimestamp_9',['copymodelwithtimestamp',['../input__output_8cpp.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp'],['../input__output_8h.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp']]]
+  ['computegranularities_4',['computegranularities',['../classGridSearchOptimizer.html#a82b0c7bbdcedb368b24aeac87388521a',1,'GridSearchOptimizer::computeGranularities()'],['../classGridSearchOptimizer.html#ad8a00101712ab87eda5f435ccf097d6e',1,'GridSearchOptimizer::computeGranularities(std::pair&lt; double, double &gt; offset_range, std::pair&lt; double, double &gt; slope_range)']]],
+  ['computemagnetellbounds_5',['computeMagnetEllBounds',['../classAbstractOptimizer.html#aea5b126ff26498a07281ffaafa1bedee',1,'AbstractOptimizer']]],
+  ['computenrmse_6',['computeNRMSE',['../classStatisticalAnalysis.html#af79acd4b85d0e9b01a590d8084ae3219',1,'StatisticalAnalysis']]],
+  ['computevariance_7',['computeVariance',['../classStatisticalAnalysis.html#a2c640b3d283bb173b9a962829023a3a7',1,'StatisticalAnalysis']]],
+  ['copymodelwithtimestamp_8',['copymodelwithtimestamp',['../input__output_8cpp.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp'],['../input__output_8h.html#a8c4ec528853f4321074612142e403108',1,'copyModelWithTimestamp(const boost::filesystem::path &amp;src_path):&#160;input_output.cpp']]]
 ];
